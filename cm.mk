b30a6625b720e3067fde3030c7df54b1ea70185a
sub77 <sub77@ymail.com>
2016-08-15 13:42:38 +0000

    No NFC

--- cm.mk
+++ cm.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/matissewifi/full_matissewifi.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
